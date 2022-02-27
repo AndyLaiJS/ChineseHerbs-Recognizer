227544c9aa4cebc44e0e1f8b01613e4137838d2c
AndyPhoenix9879 <lai9879m@gmail.com>
2022-02-27 12:46:46 +0800

    UploadButton component: leaner home.js

---
+++ fyp2021/src/components/UploadButton.js
@@ -0,0 +1,95 @@
+import styles from "./UploadButton.module.css";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+
+const UploadButton = () => {
+	const history = useHistory();
+	const [content, setContent] = useState(null);
+
+	// This some Dr. Strange stuff
+	function getBase64Image(img) {
+		var canvas = document.createElement("canvas");
+		canvas.width = img.width;
+		canvas.height = img.height;
+		var ctx = canvas.getContext("2d");
+		ctx.drawImage(img, 0, 0);
+		var dataURL = canvas.toDataURL("image/jpeg");
+		return dataURL.replace(/^data:image\/(png|jpg|jpeg);base64,/, "");
+	}
+
+	// once content is filled, go to /identified page carrying the content data
+	useEffect(() => {
+		if (content != null) {
+			history.push({
+				pathname: "/identified",
+				state: {
+					Label: content.label,
+					Desc: content.data,
+				},
+			});
+		}
+	}, [content]);
+
+	const HandleChange = (e) => {
+		e.preventDefault();
+
+		let selected = e.target.files[0];
+
+		// prepare the img for localStorage
+		var img = new Image();
+		img.src = URL.createObjectURL(selected);
+		var imgData = null;
+
+		// once img is loaded, run this function
+		img.onload = function () {
+			imgData = getBase64Image(img);
+			var ib = Buffer.from(imgData, "base64");
+			// console.log(imgData); // imgData is our base64 encoding
+			var length = imgData.length;
+			var imageBytes = new ArrayBuffer(length);
+			var ua = new Uint8Array(imageBytes);
+			for (var i = 0; i < length; i++) {
+				ua[i] = imgData.charCodeAt(i);
+			}
+			// store it somewhere for identity.js to read
+			localStorage.setItem("storedimg", imgData);
+		};
+
+		// preparing the img into a FormData format so Flask backend can process it
+		const formData = new FormData();
+		console.log(selected);
+
+		formData.append("file", selected);
+
+		// for Flask to handle
+		if (selected) {
+			axios
+				.post("http://127.0.0.1:5000/upload", formData)
+				.then(function (response, data) {
+					data = response.data;
+					setContent(data);
+					console.log(data);
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+
+			const reader = new FileReader();
+			reader.readAsDataURL(e.target.files[0]);
+		}
+	};
+
+	return (
+		<form method="post" encType="multipart/form-data">
+			<label>
+				<input type="file" onChange={HandleChange} />
+				<span className={styles.ubutton}>
+					<p>Upload</p>
+				</span>
+			</label>
+		</form>
+	);
+};
+
+export default UploadButton;
--- fyp2021/src/home.js
+++ fyp2021/src/home.js
@@ -3,113 +3,13 @@ import { useHistory } from "react-router-dom";
 import "./App.css";
 import axios from "axios";
 import CircularPicFrame from "./components/CircularPicFrame";
+import UploadButton from "./components/UploadButton";
 
 export default function Home() {
-	const history = useHistory();
-	const [content, setContent] = useState(null);
-	const [file, setFile] = useState(null);
-
-	// This some Dr. Strange stuff
-	function getBase64Image(img) {
-		var canvas = document.createElement("canvas");
-		canvas.width = img.width;
-		canvas.height = img.height;
-		var ctx = canvas.getContext("2d");
-		ctx.drawImage(img, 0, 0);
-		var dataURL = canvas.toDataURL("image/jpeg");
-		return dataURL.replace(/^data:image\/(png|jpg|jpeg);base64,/, "");
-	}
-
-	// once content is filled, go to /identified page carrying the content data
-	useEffect(() => {
-		if (content != null) {
-			history.push({
-				pathname: "/identified",
-				state: {
-					Label: content.label,
-					Desc: content.data,
-				},
-			});
-		}
-	}, [content]);
-
-	const HandleChange = (e) => {
-		let selected = e.target.files[0];
-
-		// prepare the img for localStorage
-		var img = new Image();
-		img.src = URL.createObjectURL(selected);
-		var imgData = null;
-
-		// once img is loaded, run this function
-		img.onload = function () {
-			imgData = getBase64Image(img);
-			var ib = Buffer.from(imgData, "base64");
-			// console.log(imgData); // imgData is our base64 encoding
-			var length = imgData.length;
-			var imageBytes = new ArrayBuffer(length);
-			var ua = new Uint8Array(imageBytes);
-			for (var i = 0; i < length; i++) {
-				ua[i] = imgData.charCodeAt(i);
-			}
-			// store it somewhere for identity.js to read
-			localStorage.setItem("storedimg", imgData);
-		};
-
-		e.preventDefault();
-		// preparing the img into a FormData format so Flask backend can process it
-		const formData = new FormData();
-		console.log(selected);
-
-		formData.append("file", selected);
-
-		// for Flask to handle
-		if (selected) {
-			setFile(selected);
-			axios
-				.post("http://127.0.0.1:5000/upload", formData)
-				.then(function (response, data) {
-					data = response.data;
-					setContent(data);
-					console.log(data);
-				})
-				.catch(function (error) {
-					console.log(error);
-				});
-
-			const reader = new FileReader();
-			reader.readAsDataURL(e.target.files[0]);
-		} else {
-			setFile(null);
-		}
-	};
-
 	return (
 		<div className="App">
 			<CircularPicFrame img={"/MHW2102.png"} />
-			{/* <div className="label">
-				{file && content && <div>{content.label}</div>}
-			</div>
-			<div className="desc">{file && content && <div>{content.data}</div>}</div> */}
-			<form method="post" encType="multipart/form-data">
-				<label>
-					<input type="file" onChange={HandleChange} />
-					<span className="ubutton">
-						<div className="b_inside">
-							<p
-								style={{
-									fontSize: "28px",
-									fontWeight: "bolder",
-									color: "#FF4689",
-									margin: "0",
-								}}
-							>
-								Upload
-							</p>
-						</div>
-					</span>
-				</label>
-			</form>
+			<UploadButton />
 			<footer className="footer">
 				A MHW2102 FYP Project done by:
 				<div>Lai Jian Shin, 1155116310</div>
